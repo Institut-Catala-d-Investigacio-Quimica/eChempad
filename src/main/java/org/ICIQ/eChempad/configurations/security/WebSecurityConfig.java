88c278c80034e58df8a948878a81568628590345
AleixMT <aleixaretra@gmail.com>
2022-02-25 11:43:02 +0100

    ADDED: Basic authentication, but the overridden user it is not being set up correctly. It is set to eChempad but transforms to amarine, which is the user of the database, set in another property that has no relation with it...

--- src/main/java/org/ICIQ/eChempad/configurations/security/WebSecurityConfig.java
+++ src/main/java/org/ICIQ/eChempad/configurations/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package org.ICIQ.eChempad.configurations.security;
 
+import org.ICIQ.eChempad.EChempadApplication;
 import org.ICIQ.eChempad.repositories.ResearcherRepository;
 import org.ICIQ.eChempad.services.ResearcherService;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.logging.Logger;
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,18 +25,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ResearcherService researcherService;
 
-    // Value obtained from .properties files, outside the env of the JAVA project
+    // Value obtained from .properties files, outside the env of the JAVA project. Configures Cross-site request forgery
+    // protection.
     @Value("${security.enable.csrf}")
     private boolean csrfEnabled;
+
+    @Value("${spring.security.user.name}")
+    private String admin_username;
+
+    @Value("${spring.security.user.password}")
+    private String admin_password;
+
 
     /**
      * https://stackoverflow.com/questions/2952196/ant-path-style-patterns
      * Allow everyone to access the login and logout form and allow everyone to access the login API calls.
      * Allow only authenticated users to access the API.
-     * @param http HTTP security class
+     * @param http HTTP security class. Can be used to configure a lot of different parameters regarding HTTP security.
      * @throws Exception Any type of exception that occurs during the HTTP configuration
      */
-    @Order(2)
     @Override
     protected void configure(@NotNull HttpSecurity http) throws Exception {
         // Conditional activation depending on the profile properties
@@ -41,13 +52,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             http.csrf().disable();
         }
 
+        //
         http
                 .authorizeRequests()
-                .antMatchers("/login*", "/api/researcher/**").permitAll()
-                .antMatchers("/api/journal").hasRole("USER")
-                .anyRequest().authenticated();
 
-        //http.formLogin();
+                // Publicly accessible URLs
+                .antMatchers("/login*", "/api/researcher/**").permitAll()
+
+                // You have to be authenticated and authorized to have the roll USER to access /api/journal
+                .antMatchers("/api/journal").hasRole("USER")
+
+                // The rest of requests have to be always authenticated
+                .anyRequest().authenticated()
+
+                // Creates the http form login in the default URL /login· The first parameter is a string corresponding to the
+                // URL where we will map the login form
+                .and().formLogin()
+
+                // https://stackoverflow.com/questions/57574981/what-is-httpbasic-method-in-spring-security
+                // allows the basic HTTP authentication. If the user cannot be authenticated using HTTP auth headers it
+                // will show a 401 unauthenticated
+                .and().httpBasic();
+
+
+
+        // Creates the http form login in the default URL /login· The first parameter is a string corresponding to the
+        // URL where we will map the login form
+        http.formLogin();
     }
 
 
@@ -59,8 +90,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationBuilder) throws Exception
     {
+
+        LoggerFactory.getLogger(EChempadApplication.class).info(
+                "SDFGSDFGGFGAFGSFASFASFSA" + this.admin_password + " " + this.admin_username);
+
         authenticationBuilder
                 .inMemoryAuthentication()
-                .withUser("admin").password("{noop}password").roles("USER");
+                .withUser(this.admin_username).password("{noop}" + this.admin_password).roles("USER");
     }
 }
