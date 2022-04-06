50d2ceeb875725f87241bac7f28749329ecb6c6e
AleixMT <aleixaretra@gmail.com>
2022-04-06 11:39:04 +0200

    UPDATED: project structure and auto setup script

--- src/main/resources/Automatic_setup/setup.sh
+++ src/main/resources/Automatic_setup/setup.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 #
 # |===================================================================================|
@@ -23,7 +22,21 @@
 
 main()
 {
+  # Create the database template for eChempad
   createdb -E UTF8 --locale='en_US.utf8' -T template0 -O $(whoami) "eChempad"
+
+  # Create the user amarine for PostGreSQL access. Give all permissions and the default password "chemistry"
+  sudo -u postgres createuser --interactive --pwprompt
+
+  # Create the file_db folder and make it accessible for the current user (which is expected to be the one running
+  # eChempad)
+  sudo mkdir -p /home/amarine/Desktop/eChempad/file_db
+  sudo chmod 777 /home/amarine/Desktop/eChempad/file_db
+  sudo chown $(whoami):$(whoami) /home/amarine/Desktop/eChempad/file_db
+
+  # Create two hardcoded files in the DB (needed for the dummy initialization)
+  echo "LICENSE" > /home/amarine/Desktop/eChempad/file_db/LICENSE.md
+  echo "Photo which is text" > /home/amarine/Desktop/eChempad/file_db/3.jpg
 
 }
 
