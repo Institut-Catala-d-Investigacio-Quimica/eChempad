57728502f81a3ac730bd2628d8e0b42f7461f1d5
AleixMT <aleixaretra@gmail.com>
2022-04-06 10:07:10 +0200

    ADDED: Docuument helper class

---
+++ src/main/java/org/ICIQ/eChempad/configurations/PathConverter.java
@@ -0,0 +1,22 @@
+package org.ICIQ.eChempad.configurations;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Converter(autoApply = true)  // With autoapply = true performs automatic translation between types implicitly
+public class PathConverter implements AttributeConverter<Path, String>, Serializable {
+
+    @Override
+    public String convertToDatabaseColumn(Path path) {
+        return path == null ? null : path.toString();
+    }
+
+    @Override
+    public Path convertToEntityAttribute(String s) {
+        return s == null ? null : Paths.get(s);
+    }
+}
--- src/main/java/org/ICIQ/eChempad/services/ExperimentServiceClass.java
+++ src/main/java/org/ICIQ/eChempad/services/ExperimentServiceClass.java
@@ -15,7 +15,6 @@ import org.ICIQ.eChempad.exceptions.ResourceNotExistsException;
 import org.ICIQ.eChempad.repositories.ExperimentRepository;
 import org.ICIQ.eChempad.repositories.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
