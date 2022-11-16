1e6ee87d51d82fa970c8b9097aaa92b62e350ce0
AleixMT <aleixaretra@gmail.com>
2022-11-16 14:28:02 +0100

    UPDATED: file upload done, now metadata upload required

--- src/main/java/org/ICIQ/eChempad/services/DataverseExportServiceImpl.java
+++ src/main/java/org/ICIQ/eChempad/services/DataverseExportServiceImpl.java
@@ -3,10 +3,10 @@ package org.ICIQ.eChempad.services;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.researchspace.dataverse.api.v1.DataverseAPI;
 import com.researchspace.dataverse.api.v1.DataverseConfig;
-import com.researchspace.dataverse.entities.DataverseGet;
+import com.researchspace.dataverse.entities.Dataset;
 import com.researchspace.dataverse.entities.Identifier;
-import com.researchspace.dataverse.entities.facade.DatasetFacade;
 import com.researchspace.dataverse.http.DataverseAPIImpl;
+import com.researchspace.dataverse.sword.FileUploader;
 import org.ICIQ.eChempad.configurations.converters.DocumentWrapperConverter;
 import org.ICIQ.eChempad.configurations.wrappers.DataverseDatasetMetadata;
 import org.ICIQ.eChempad.configurations.wrappers.DataverseDatasetMetadataImpl;
@@ -19,24 +19,29 @@ import org.ICIQ.eChempad.entities.genericJPAEntities.Researcher;
 import org.ICIQ.eChempad.services.genericJPAServices.DocumentService;
 import org.ICIQ.eChempad.services.genericJPAServices.ExperimentService;
 import org.ICIQ.eChempad.services.genericJPAServices.JournalService;
+import org.apache.abdera.i18n.iri.IRI;
+import org.apache.abdera.model.IRIElement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.swordapp.client.*;
 
-import javax.print.Doc;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.*;
 import java.util.logging.Logger;
+
+import static com.researchspace.dataverse.entities.facade.PublicationIDType.doi;
 
 /**
  * Implementation of class to export data to a running Dataverse instance.
@@ -119,26 +124,57 @@ public class DataverseExportServiceImpl implements DataverseExportService {
 
         // Call Dataverse API client to create dataset into the ICIQ dataverse
         Identifier identifier = api.getDataverseOperations().createDataset(dataverseDatasetMetadata.toString(), "ICIQ");
-        Logger.getGlobal().warning("THE IDENTIFIER " + identifier);
-        DataverseGet dataverseGet = api.getDataverseOperations().getDataverseById(identifier.toString());
-        Logger.getGlobal().warning("THE DATASET GET " + dataverseGet);
-        String idCreatedDataset = dataverseGet.getId();
-        Logger.getGlobal().warning("THE DATASET id " + idCreatedDataset);
+        Logger.getGlobal().warning("A dataset with identifier " + identifier.getId() + "has been created into the ICIQ dataverse.");
 
         // Upload files of each experiment into the created dataset
         // Get all experiments from selected journal
         for (Experiment experiment: this.experimentService.getExperimentsFromJournal((UUID) id))
         {
-            // Get all documents for each experiments
+            // Get all documents for each experiment
             for (Document document: this.documentService.getDocumentsFromExperiment(((UUID) experiment.getId())))
             {
-                // Obtain DocumentWrapper from the current selected Document
-                DocumentWrapper documentWrapper = this.documentWrapperConverter.convertToEntityAttribute(document);
-
-                Logger.getGlobal().warning(documentWrapper.getId().toString());
                 try {
+                    /*
+                     * Obtain DocumentWrapper from the currently selected Document in order to obtain a MultipartFile.
+                     * This will be transformed into a File in order to fulfill the parameters of the uploadFile method.
+                     * TODO When the PR of the repository is finished, we will be able to use a InputStream to upload a
+                     *  File, so we do not have to transform it into DocumentWrapper or having to bulk the data into the
+                     *  disk before uploading it.
+                     */
+                    DocumentWrapper documentWrapper = this.documentWrapperConverter.convertToEntityAttribute(document);
                     File file = DataverseExportServiceImpl.multipartToFile(documentWrapper.getFile(), documentWrapper.getId().toString());
-                    api.getDatasetOperations().uploadFile(idCreatedDataset, file);
+
+                    // Obtain the dataset what we just created in order to obtain its DOI.
+                    Dataset dataset = api.getDatasetOperations().getDataset(identifier);
+
+                    // If we have a valid dataset, then we can
+                    if (! dataset.getDoiId().isPresent())
+                    {
+                        // TODO throw exception
+                        throw new RuntimeException();
+                    }
+                    else
+                    {
+                        // This line could do the job but the library is quite unuseful until my PR are merged back into the base repository.
+                        // api.getDatasetOperations().uploadFile(dataset.getDoiId().get(), file);
+                        // Instead, we unwrap the function and get the return info.
+                        DepositReceipt depositReceipt = this.uploadFile(APIKey, dataset.getDoiId().get(), file);
+
+                        // This code retrieves the identifier from SWORD library, which is actually a full URL to the
+                        // uploaded file, which includes its DOI.
+                        // With this code we trim the DOI from the link, which actually hurts me as software developer
+                        // and user of this quite unuseful library.
+                        String[] result = depositReceipt.getEntry().getId().toString().split("/");
+                        String fileDOI = result[result.length - 2] + "/" + result[result.length - 1];
+
+                        Logger.getGlobal().warning("iriDB is " + fileDOI);
+
+                        // Obtain autogenerated file metadata in order to update it with metadata from the instances in
+                        // echempad
+                        ObjectNode objectNode = this.getFileMetadata(APIKey, fileDOI);
+                        Logger.getGlobal().warning("metadata from object node is " + objectNode.toPrettyString());
+
+                    }
                 } catch (IOException e) {
                     // TODO throw exception
                     e.printStackTrace();
@@ -158,6 +194,34 @@ public class DataverseExportServiceImpl implements DataverseExportService {
     @Override
     public String exportJournal(Serializable id) throws IOException {
         return exportJournal( ((UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getResearcher().getDataverseAPIKey(), id);
+    }
+
+    public ObjectNode getFileMetadata(String APIKey, String fileDOI) throws IOException
+    {
+        return this.webClient
+                .get()
+                .uri(DataverseExportServiceImpl.baseURL + "/api/files/:persistentId/metadata?persistentId=" + fileDOI)
+                .header("X-Dataverse-key", APIKey)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(ObjectNode.class)
+                .block();
+    }
+
+    public DepositReceipt uploadFile(String APIKey, String dataset_DOI, File file)
+    {
+        FileUploader uploader = new FileUploader();
+        try {
+            return uploader.deposit(file, APIKey, new URI(DataverseExportServiceImpl.baseURL), dataset_DOI);
+        } catch (IOException | SWORDClientException  | ProtocolViolationException | URISyntaxException e)
+        {
+            Logger.getGlobal().severe("Couldn't upload file " + file.getName() + " with doi " + doi.toString() + ": " + e.getMessage());
+            throw new RestClientException(e.getMessage());
+        } catch (SWORDError error)
+        {
+            Logger.getGlobal().severe("SwordError: " + error.getErrorBody());
+            throw new RestClientException(error.getErrorBody());
+        }
     }
 
     @Override
